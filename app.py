34ede539c119cda2da4a7de8957a6c9371e549b0
Sifr'un <36736908+Rayryu@users.noreply.github.com>
2023-05-27 16:37:40 +0000

    Add document upload and content reading

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-def main():
-    print("Here we go")
+import streamlit as st
+from PyPDF2 import PdfReader
 
-if __name__ == '__main__':
+
+def main():
+    st.set_page_config(page_title="Ask your document")
+    st.header("Ask your document 🤔")
+
+    # Upload the document
+    document = st.file_uploader(
+        "Upload your document", type="pdf"
+    )  # TODO: I'll start with pdf then maybe add other types
+
+    # Read the document
+    if document is not None:
+        pdf_reader = PdfReader(document)
+
+        content = ""
+        for page in pdf_reader.pages:
+            content += page.extract_text()
+
+        print(content)
+
+
+if __name__ == "__main__":
     main()
