467b2f1a923b874f14d9d9c7da8a971f94fbd937
Sifr'un <36736908+Rayryu@users.noreply.github.com>
2023-06-05 21:37:45 +0000

    Update UI

--- html_templates.py
+++ html_templates.py
@@ -1,44 +1,56 @@
 css = """
 <style>
-.chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
-}
-.chat-message.user {
-    background-color: #2b313e
-}
-.chat-message.bot {
-    background-color: #475063
-}
-.chat-message .avatar {
-  width: 20%;
-}
-.chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
-}
-.chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
-}
+  .chat-message {
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    margin-bottom: 0.5rem;
+    display: flex;
+  }
+
+  .chat-message.user {
+    background-color: #2b313e;
+  }
+
+  .chat-message.bot {
+    background-color: #475063;
+  }
+
+  .chat-message .avatar {
+    width: 20%;
+  }
+
+  .chat-message .avatar img {
+    max-width: 10px;
+    max-height: 10px;
+    border-radius: 90%;
+    object-fit: cover;
+  }
+
+  .chat-message .message {
+    width: 95%;
+    padding: 0 1.5rem;
+    color: #fff;
+  }
+</style>
+
 """
 
 bot_template = """
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://purepng.com/public/uploads/large/purepng.com-robotrobotprogrammableautomatonelectronics-1701528369303t90mq.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://purepng.com/public/uploads/large/purepng.com-robotrobotprogrammableautomatonelectronics-1701528369303t90mq.png" style="max-height: 90px; max-width: 50px; border-radius: 5%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
+
 """
 
 user_template = """
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://purepng.com/public/uploads/large/purepng.com-user-iconsymbolsiconsapple-iosiosios-8-iconsios-8-721522596134a4bio.png">
+        <img src="https://purepng.com/public/uploads/large/purepng.com-user-iconsymbolsiconsapple-iosiosios-8-iconsios-8-721522596134a4bio.png" style="max-height: 90px; max-width: 40px; border-radius: 5%; object-fit: cover;">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
+
 """
